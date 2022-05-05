9b5d5481e913e80ce168dc9b2b186641388d5dac
gploc <gploc@DESKTOP-6LUVO7C>
2022-05-05 20:56:56 +0200

    Mock deleted

--- app/filter/filter.service.js
+++ app/filter/filter.service.js
@@ -1,11 +1,5 @@
 const Notice = require('../notice/notice');
 
-const items = [
-    { id: 1, name: 'example', tags: ["tag1", "tag2"] },
-    { id: 2, name: 'example1', tags: ["tag1", "tag2"] },
-    { id: 3, name: 'example2', tags: ["tag3", "tag4"] }
-]
-
 module.exports = { getFiltered, getAll }
 
 async function getFiltered(tag) {
